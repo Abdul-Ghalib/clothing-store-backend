9ec5dd7fc302d1bd3c719f0c523f321d3ec7205d
Abdulghalib Adebola <deybola_xm@Deybolas-MacBook-Pro.local>
2026-06-16 09:31:05 +0100

    folder structure

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes import
+import userRouter from "./routes/user-routes.js";
+import postRouter from "./routes/post-routes.js";
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/users", postRouter);
+
+// example route: http://localhost:4000/api/v1/users/register
+
+export default app;
---
+++ backend/src/routes/user-routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { registerUser, loginUser, logoutUser } from "../controllers/user-controller.js";
+
+const userRouter = Router();
+
+userRouter.route('/register').post(registerUser);
+userRouter.route('/login').post(loginUser);
+userRouter.route('/logout').post(logoutUser);
+
+
+export default userRouter;
